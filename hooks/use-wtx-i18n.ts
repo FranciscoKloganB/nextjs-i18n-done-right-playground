6f8c4f3369465f57f4acadf246a6d9d066d92b0e
romansharapov19 <r.sharapov@kigroup.de>
2022-07-19 14:41:28 +0100

    feat(dinamic-translations): config _document file for translations

--- hooks/use-wtx-i18n.ts
+++ hooks/use-wtx-i18n.ts
@@ -5,7 +5,7 @@ import { useTranslation } from "react-i18next"
 export default function useWtxI18n() {
   const { asPath, locale, pathname, replace, query } = useRouter()
   const {
-    i18n: { language }
+    i18n: { dir, language }
   } = useTranslation()
 
   /**
@@ -20,6 +20,8 @@ export default function useWtxI18n() {
     console.debug("Hook useWtxI18n executed", { old: `${locale}`, new: `${language}` })
     // Update URL 'locale' (e.g.: /de/* to /ar/*) when dropdown item is clicked
     replace({ pathname, query }, asPath, { locale: language })
+
+    document.dir = dir()
     // Inform Eslint that we intend to re-run the effect, if and only if, language changes
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [language])
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,36 @@
 import React from "react"
-import Document, { Html, Head, Main, NextScript } from "next/document"
+import { getCookie } from "cookies-next"
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document"
+import i18n, { defaultLanguage, supportedLanguages } from "../i18n.config"
+
+function selectLocale(ctx: DocumentContext): string {
+  let prioritizedLanguage = defaultLanguage
+
+  const cookieLanguage = getCookie("i18next", { req: ctx.req })?.toString()
+  if (cookieLanguage && supportedLanguages.includes(cookieLanguage)) {
+    prioritizedLanguage = cookieLanguage
+  }
+
+  const urlLanguage = ctx.locale
+  if (urlLanguage && supportedLanguages.includes(urlLanguage)) {
+    prioritizedLanguage = urlLanguage
+  }
+
+  return prioritizedLanguage
+}
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return { ...initialProps, locale: selectLocale(ctx) }
+  }
+
   render() {
+    const locale = this.props.locale
+
     return (
-      <Html lang="en">
+      <Html lang={locale} dir={i18n.dir(locale)}>
         <Head />
         <body>
           <Main />
