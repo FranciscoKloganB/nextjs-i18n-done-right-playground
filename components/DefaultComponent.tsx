ccb81e0368251dce5481c01ac6b7d58306f4e04b
romansharapov19 <r.sharapov@kigroup.de>
2022-07-19 11:43:26 +0100

    feat(default-ui): add minor changes to the default component ui

--- components/DefaultComponent.tsx
+++ components/DefaultComponent.tsx
@@ -6,7 +6,7 @@ const projects = [
 
 export default function DefaultComponent() {
   return (
-    <div className="flex w-4/5 flex-col justify-center px-4 sm:px-6 lg:px-8">
+    <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
           <h1 className="text-xl font-semibold text-gray-900">Invoice</h1>
